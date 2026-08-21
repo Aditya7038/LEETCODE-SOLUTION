27c5b943cb1807ad4ea1ae5e47d2a7e886d42992
Aditya7038 <rayatuwaraditya@gmail.com>
2026-08-21 21:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (13.03%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,9 +1,16 @@
+static const int speedup = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-
-        for (int i = 1; i < nums.size(); i++) nums[i] += nums[i - 1];
-                  return nums;
-        
+        int n = nums.size();
+        for (int i = 1; i < n; ++i) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
     }
 };
