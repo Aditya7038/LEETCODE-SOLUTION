6a8740d1524b919df92608ff631675e3005f0897
Aditya7038 <rayatuwaraditya@gmail.com>
2026-08-30 23:33:07 +0530

    Time: 25 ms (13.54%) | Memory: 26.4 MB (16.01%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -2,40 +2,41 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
 
-        int n = arr.size();
 
-        if(n<=2) return false;
+        int j = arr.size()-1;
+
+        int i =0;
 
         bool flag1 = false;
-
         bool flag2 = false;
 
-        for (int i = 0;i<n-1; ){
+        if(arr.size()<3) return false;
 
-           
+        for (;i<j;){
 
-            if(arr[i+1]>arr[i]){
+            if(arr[i+1]>arr[i]) i++;
 
-                if (flag2 == true) return false;
+            else flag1 = true;
 
-                flag1 = true;
-                if(i+1==n-1) return false;
+            if(arr[j-1]>arr[j]) j--;
 
-                i++;
-            }
+            else flag2 = true;
 
-            else{
+            if(j==arr.size()-1) return false; // increasing arr only
 
-                flag2 = true;
+            if(i == 0 ) return false; // decreasing arr only
 
-                if(flag1 == false) return false;
+            if (flag1 == true && flag2 == true) break;
 
-                if(arr[i+1]>=arr[i]) return false;
-                else i++;
+            
 
-            }
         }
 
-        return true;
+        if(i==j) return true;
+
+
+        return false;
+
+        
     }
 };
