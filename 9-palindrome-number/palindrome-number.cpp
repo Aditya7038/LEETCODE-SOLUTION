cd65c1ef6045d04041f603c2cc03fd76e31487fd
Aditya7038 <rayatuwaraditya@gmail.com>
2026-08-31 21:52:32 +0530

    Time: 4 ms (30.49%) | Memory: 10.9 MB (7.01%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        string s = to_string(x);
+
+        int n = s.size();
+
+        int i = 0 ;
+        int j = n-1;
+
+        for(; i<j;){
+
+            if(s[i]==s[j]) i++,j--;
+
+            else return false;
+        }
+
+
+        return true;
+        
+    }
+};
