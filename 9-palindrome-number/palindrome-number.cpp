64f2e4408bbfe687cb3a254903bd321072a6216e
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-01 22:23:14 +0530

    Time: 2 ms (50.06%) | Memory: 8.5 MB (64.76%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -6,7 +6,17 @@ public:
 
         if(x<0) return false;
 
-        int n = log10(x) + 1;
+        int y =x;
+
+       // int n = log10(x) + 1; // count digits , dont trust log10 because -> it works with floating pt internally for log10(100) it can ans 2.999999999.
+        
+        int n = 0;
+        for (int i =0;y!=0;i++){
+
+            y = y/10;
+
+            n++;
+        }
 
         if(n==1) return true;
 
@@ -24,7 +34,7 @@ public:
  
         }
 
-        if(x == reverse &&  n%2==0) return true;
+        if(  n%2==0 && x == reverse ) return true;
 
         else if( n%2 !=0 && x/10 == reverse  ) return true;
 
