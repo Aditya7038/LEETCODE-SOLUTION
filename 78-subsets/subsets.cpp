39296d943dd035e1357dab647678fa606e864d2d
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-05 14:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (59.52%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,29 +1,34 @@
+   void helper( int idx ,vector<int> &nums, vector<int> &empty, vector<vector<int>> &v1){
+
+    if(idx == nums.size()){
+        v1.push_back(empty);
+        return;
+    }
+
+    empty.push_back(nums[idx]);
+
+    helper(idx+1, nums, empty , v1);
+
+    empty.pop_back();
+
+    helper(idx+1, nums, empty , v1);
+
+
+}
+
+
+
 class Solution {
 public:
-    void helper(vector<int> ans, vector<int> nums, vector<vector<int>> &v1){
 
-        if(nums.size() == 0){
-            v1.push_back(ans);
-            return;
-        }
 
-        vector<int> nums2(nums.begin()+1, nums.end());
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> v1;
+    vector<int> empty;
+    helper(0, nums, empty, v1);
+    return v1;
+}
 
-        vector<int> ans2 = ans;
-        ans2.push_back(nums[0]);
-
-        helper(ans2, nums2, v1);   // include nums[0]
-
-        helper(ans, nums2, v1);    // exclude nums[0]
-    }
-
-    vector<vector<int>> subsets(vector<int>& nums) {
-
-        vector<vector<int>> v1;
-        vector<int> ans;
-
-        helper(ans, nums, v1);
-
-        return v1;
-    }
+        
+    
 };
