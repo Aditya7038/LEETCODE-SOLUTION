4635702a2fef4a3fee775f0594295c1901fe3924
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-05 20:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (65.45%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int fib(int n) {
 
-        
+        if(n == 0) return 0;
+        if(n == 1) return 1;
 
-    if(n== 0) return 0;
-    if(n== 1) return 1;
+        int thirdmax = 0;  
+        int secmax = 1;    
+        int max = 0;
 
-    return fib(n-1)+ fib(n-2);
+        for(int i = 0; i < n-1; i++){
+            max = secmax + thirdmax;
+            thirdmax = secmax;
+            secmax = max;
+        }
 
-    
-    
-        
+        return max;
     }
 };
