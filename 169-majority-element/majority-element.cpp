6e26862c5c4558d766d528da91e8959da476f1c9
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-08 23:31:28 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (95.11%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,44 +4,21 @@ public:
 
         int n = nums.size();
 
-        if(n==1 || n==2) return nums[0];
+        int count = 0;
+        int candidate;
 
+        for(int i = 0; i<n;i++){
 
-        int x ;
-
-        if(n%2==0) x = n/2;
-
-        else x = (n/2) + 1;
-
-        int count = 1;
-
-        int temp;
-
-
-        for(int i =0;i<x;i++){
-
-            count = 1;
-
-            for (int j = i+1 ; j< n;j++){
-
-                if(nums[i]==nums[j]) count++;
-
-                if(count>n/2)  {
-                    temp = nums[i];
-
-                    return nums[i];
-                    break;
-                    }
-
+            if(count==0) {
+                candidate  = nums[i];
             }
+                
+            if(candidate==nums[i]) count++;
+
+            if(candidate!=nums[i]) count--;
 
         }
-
-        return nums[temp];
-
-
-
-
+        return candidate;
         
     }
 };
