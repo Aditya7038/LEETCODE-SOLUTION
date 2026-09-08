eebbce2cbb8454c9dfe9bcd73a8e19e4dc8fe111
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-09 01:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (45.51%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,54 +1,16 @@
-int count_digit(int num){
 
-    int count =0;
-
-    for(int i =0 ; num!=0;i++){
-
-        num = num/10;
-        count++;
-    }
-
-    return count;
-}
-
-
-int  helper (int num,int sum){
-
-    int len = count_digit(num);
-
-    if(len > 1){
-
-        for(int i = 0 ; i<len;i++){
-
-        sum = sum + num % 10;
-
-        num = num / 10;
-
-        }
-        num = sum;
-
-        return helper(num , 0);
-
-    }
-
-    else return num;
-
-}
 
 
 class Solution {
 public:
     int addDigits(int num) {
 
-    if(num==0) return 0;
+        if(num==0) return 0;
 
-    int len = count_digit(num) ;
+        if(num%9 != 0) return num%9;
 
-    if(len==1) return num;
+        else{ return 9;}
 
-        int y = helper(num,0);
-
-        return y;
         
     }
 };
