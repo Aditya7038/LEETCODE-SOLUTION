030f88c067f680de2fb7b3162b1d2974824d4d89
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-11 00:03:05 +0530

    Time: 1 ms (9.61%) | Memory: 9.1 MB (18.47%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     long long countCommas(long long n) {
+        if (n < 1000) return 0;
 
-        if(n<1000) return 0;
+        long long x = 1, y = 1000, comma = 0;
+        long long multiplier = 0;
 
-        long long x = 1 ,y = 1000,comma = 0;
-
-        int multiplier = 0;
-
-
-        for (;;){
- 
-
-            if(n>=x && n<y){
-
-              return (n - x + 1) * multiplier + comma;
-
-                break;
+        for (;;) {
+            if (n >= x && n < y) {
+                return (n - x + 1) * multiplier + comma;
+            } else {
+                comma += (y - x) * multiplier;  // half-open range [x, y): count is y - x
+                multiplier++;
+                x *= 1000;
+                y *= 1000;
             }
-
-            else{
-
-            comma = comma + (y-x) * multiplier;
-             multiplier++;
-                x = x*pow(10,3);
-                y = y*pow(10,3);
-            }
-                
-            
-
-         
-
         }
-
-
-
     }
 };
