5dba5ebbcc4246be7f9d2344e64382a3c8d7215f
Aditya7038 <rayatuwaraditya@gmail.com>
2026-09-20 22:12:09 +0530

    Time: 2 ms (43.77%) | Memory: 11.6 MB (83.10%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        vector<string> A;
+
+        for(int i =1; i<=n;i++){
+
+            if(i%3==0 && i%5==0){
+
+                A.push_back("FizzBuzz");
+                continue;
+            }
+
+            if(i%3==0){
+
+                A.push_back("Fizz");;
+                continue;
+            }
+
+            if(i%5==0){
+
+                A.push_back("Buzz");
+                continue;
+            }
+
+            else{
+
+                A.push_back(to_string(i));
+            }
+        }
+
+        return A;
+        
+    }
+};
